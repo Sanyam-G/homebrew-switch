3231af63c325f123b6b3f52cd177c1d15574937c
Sanyam G <sanyamg2006@gmail.com>
2026-05-17 01:13:30 +0400

    Switch 0.2.4

--- Casks/switch.rb
+++ Casks/switch.rb
@@ -1,6 +1,6 @@
 cask "switch" do
-  version "0.2.3"
-  sha256 "cd8d72b046e544d00071be10202bba22b95ae28fe0633b0aaebae49ee073b3bd"
+  version "0.2.4"
+  sha256 "1efc60dd78984e193391e507f5216a54975c0d8a44a9594da8997022dc545f9e"
 
   url "https://github.com/Sanyam-G/switch/releases/download/v#{version}/Switch-#{version}.dmg",
       verified: "github.com/Sanyam-G/switch/"
